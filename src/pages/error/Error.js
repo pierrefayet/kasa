34fe04be143a06b622cc9494b245272369cb2614
pierre.fayet <piero69450@gmail.com>
2023-02-16 03:36:31 +0100

    Added error page design

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './error.scss';
 import Header from '../../component/header/Header';
 
-function Error() {
+export default function Error() {
   return (
-    <div>
+    <div className='errorPage'>
       <Header />
-      <div>
-        <h1>404</h1></div>
-    </div>
+        <h1 className='errorPage_content_title'>404</h1>
+        <p className='errorPage_content_message'>Oups! La page que vous demandez n'existe pas.</p>  
+        <p className='errorPageReturn'>Retourner a la page d'accueil</p>      
+      </div>
+    
   );
 }
 
-export default Error;
