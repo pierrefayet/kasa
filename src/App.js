d97b7471197b82a4854f20f6ab12194070b3529f
pierre.fayet <piero69450@gmail.com>
2023-02-25 21:07:11 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Homepage from './pages/homepage/Homepage';
 import Error from './pages/error/Error';
 import About from './pages/about/About';
 import Accomodation from "./pages/accomodation/Accomodation";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { rootPath, accomodationPath, aboutPath } from "./routes";
-const router = createBrowserRouter([
+const router = createHashRouter([
 	{
 		path: rootPath,
 		element: <Homepage />,
